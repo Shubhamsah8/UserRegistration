000a12fefbfb0a99945b5f02a04de4d6d919da18
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-29 13:14:37 +0530

    Delete src/main/java/org/example/FirstNameValidator.java

--- src/main/java/org/example/FirstNameValidator.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class FirstNameValidator{
-    public static boolean isValidFirstName(String firstName){
-        return firstName.matches("[A-Z]{1}[a-z]{2,}");
-    }
-}
