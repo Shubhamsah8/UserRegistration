724b5692915e877d3608cd3543811713a42bb988
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-29 15:33:27 +0530

    Delete src/main/java/org/example/EmailValidator.java

--- src/main/java/org/example/EmailValidator.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class EmailValidator{
-    public static boolean isValidateEmail(String email){
-        return email.matches("[a-zA-Z0-9./%]+@[A-Za-z]+\\.[a-zA-Z]{2,}");
-    }
-}
