f9d7c37e4f284c66a88f6a5a9917017eba3f356f
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-29 15:36:19 +0530

    Add files via upload

--- src/test/java/UserValidationTest.java
+++ src/test/java/UserValidationTest.java
@@ -6,10 +6,51 @@ import org.junit.jupiter.api.Test;
 import org.junit.Assert.*;
 public class UserValidationTest {
 
+    UserValidation userValidation = new UserValidation();
     @Test
     public void testValidFirstName(){
-        UserValidation userValidation = new UserValidation();
+        try {
+            Assert.assertTrue(userValidation.isValidFirstName("Shubham"));
+        } catch (UserValidation.InvalidUserException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-        Assert.assertTrue(userValidation.isValidFirstName("Shubham"));
+    @Test
+    public void testValidLastName(){
+
+        try {
+            Assert.assertTrue(userValidation.isValidLastName("Shah"));
+        } catch (UserValidation.InvalidUserException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void testValidEmail(){
+
+        try {
+            Assert.assertTrue(userValidation.isValidEmail("shubhamsah086@gmail.com"));
+        } catch (UserValidation.InvalidUserException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void testValidMobileNumber(){
+        try {
+            Assert.assertTrue(userValidation.isValidMobileNumber("91 8779817254"));
+        } catch (UserValidation.InvalidUserException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void testValidPassword(){
+        try {
+            Assert.assertTrue(userValidation.isValidPassword("SHubhamsha123#"));
+        } catch (UserValidation.InvalidUserException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
