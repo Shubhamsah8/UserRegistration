83772af3c5f9cceba75975feeef157a8bc429e01
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-29 15:31:04 +0530

    Add files via upload

--- src/main/java/org/example/UserValidation.java
+++ src/main/java/org/example/UserValidation.java
@@ -1,29 +1,60 @@
 package org.example;
 
+
 public class UserValidation {
+    public class InvalidUserException extends Exception{
+        InvalidUserException(String s){
+            super(s);
+        }
+    }
 
     //First Use case of valid First name
-    public boolean isValidFirstName(String firstName){
-        return firstName.matches("[A-Z]{1}[a-z]{2,}");
+    public boolean isValidFirstName(String firstName) throws InvalidUserException{
+        if(firstName.matches("[A-Z]{1}[a-z]{2,}")){
+            return true;
+        }
+        else{
+            throw new InvalidUserException("Invalid First Name");
+        }
     }
 
-    public boolean isValidLastName(String lastName){
-        return lastName.matches("[A-Z][a-z]{2,}");
+    public boolean isValidLastName(String lastName) throws InvalidUserException{
+        if(lastName.matches("[A-Z][a-z]{2,}")){
+            return true;
+        }
+        else{
+            throw new InvalidUserException("Invalid Last Name");
+        }
     }
 
-    public boolean isValidEmail(String email){
-        return email.matches("[a-zA-Z0-9./%]+@[A-Za-z]+\\.[a-zA-Z]{2,}");
+    public boolean isValidEmail(String email) throws InvalidUserException{
+        if(email.matches("[a-zA-Z0-9./%]+@[A-Za-z]+\\.[a-zA-Z]{2,}")){
+            return true;
+        }
+        else{
+            throw new InvalidUserException("Invalid Email");
+        }
     }
 
-    public static boolean isValidMobileNumber(String mobileNumber){
-        return mobileNumber.matches("91 \\d{10}");
+    public boolean isValidMobileNumber(String mobileNumber) throws InvalidUserException{
+        if(mobileNumber.matches("91 \\d{10}")){
+            return true;
+        }
+        else{
+            throw new InvalidUserException("Invalid Mobile Number");
+        }
     }
 
-    public static boolean isValidPassword(String password){
-        return password.length() >=8 &&
+    public boolean isValidPassword(String password) throws InvalidUserException{
+        if(password.length() >=8 &&
                 password.matches(".*[A-Z]*.") &&
                 password.matches(".*[//d{10}]*.") &&
-                password.matches(".*[!@#$%&*()_+]*.");
+                password.matches(".*[!@#$%&*()_+]*.")){
+            return true;
+        }
+        else{
+            throw new InvalidUserException("Invalid Password");
+        }
 
     }
 }
