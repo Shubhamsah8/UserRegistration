435ec1cb08766f1d4b4b0cbe01b78e337cc62dc8
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-29 13:44:37 +0530

    Add files via upload

--- src/test/java/UserValidationTest.java
+++ src/test/java/UserValidationTest.java
@@ -32,6 +32,6 @@ public class UserValidationTest {
 
     @Test
     public void testValidPassword(){
-        Assert.assertTrue(userValidation.isValidPassword("Shubhamsha"));
+        Assert.assertTrue(userValidation.isValidPassword("SHubhamsha"));
     }
 }
