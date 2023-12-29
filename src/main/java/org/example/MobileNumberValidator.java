06c42e4fafc645bdb3c5069d4e553b10ff2503d7
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-29 15:34:02 +0530

    Delete src/main/java/org/example/MobileNumberValidator.java

--- src/main/java/org/example/MobileNumberValidator.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class MobileNumberValidator{
-    public static boolean isValidateMobileNumber(String mobileNumber){
-        return mobileNumber.matches("91 \\d{10}");
-    }
-}
