60ef922bff082c0e360a6f6b8a6152fdd7e8a216
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-29 13:20:53 +0530

    Add files via upload

--- src/main/java/org/example/UserValidation.java
+++ src/main/java/org/example/UserValidation.java
@@ -7,5 +7,9 @@ public class UserValidation {
         return firstName.matches("[A-Z]{1}[a-z]{2,}");
     }
 
+    public boolean isValidLastName(String lastName){
+        return lastName.matches("[A-Z][a-z]{2,}");
+    }
+
 
 }
