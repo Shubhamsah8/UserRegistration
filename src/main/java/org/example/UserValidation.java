ff8fb015df67169dfb0642ed7264988d362d88c3
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-29 14:02:02 +0530

    Add files via upload

--- src/main/java/org/example/UserValidation.java
+++ src/main/java/org/example/UserValidation.java
@@ -21,6 +21,7 @@ public class UserValidation {
 
     public static boolean isValidPassword(String password){
         return password.length() >=8 &&
-                password.matches(".*[A-Z]*.");
+                password.matches(".*[A-Z]*.") &&
+                password.matches(".*[//d{10}]*.");
     }
 }
