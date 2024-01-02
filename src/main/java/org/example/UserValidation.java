a6461a33f1fc88af8fe41c697a433f79ac1476ff
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2024-01-02 09:11:36 +0530

    Update UserValidation.java

--- src/main/java/org/example/UserValidation.java
+++ src/main/java/org/example/UserValidation.java
@@ -1,11 +1,17 @@
 package org.example;
 
+/*
+    @description: This class handles user validation operations.
+*/
 public class UserValidation {
 
-    //First Use case of valid First name
+    /*
+        @description: Validates the first name according to specified criteria.
+        @parameters:
+            - firstName: The first name to be validated.
+        @return: Returns true if the first name is valid, false otherwise.
+    */
     public boolean isValidFirstName(String firstName){
         return firstName.matches("[A-Z]{1}[a-z]{2,}");
     }
-
-
 }
