41d942c885a55f155030071cd6130da78cb28f03
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2024-01-02 09:12:39 +0530

    Update UserValidationTest.java

--- src/test/java/UserValidationTest.java
+++ src/test/java/UserValidationTest.java
@@ -4,12 +4,27 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.Assert.*;
+
+/*
+    @description: This class contains test cases for user validation.
+*/
 public class UserValidationTest {
 
+    /*
+        @description: Tests the validation of a valid first name.
+        @parameters: None
+        @return: None
+    */
     @Test
     public void testValidFirstName(){
         UserValidation userValidation = new UserValidation();
 
+        /*
+            @description: Validates a first name "Shubham".
+            @parameters:
+                - "Shubham": The first name to be validated.
+            @return: Expects the validation result to be true.
+        */
         Assert.assertTrue(userValidation.isValidFirstName("Shubham"));
     }
 }
