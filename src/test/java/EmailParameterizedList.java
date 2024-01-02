55c49d5a950ad92f533da2b1cbb595da9ce41435
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2024-01-02 09:39:44 +0530

    Update EmailParameterizedList.java

--- src/test/java/EmailParameterizedList.java
+++ src/test/java/EmailParameterizedList.java
@@ -8,14 +8,27 @@ import org.junit.runners.Parameterized.Parameters;
 import java.util.Arrays;
 import java.util.List;
 
+/*
+    @description: This class contains parameterized tests for email validation.
+*/
 @RunWith(Parameterized.class)
 public class EmailParameterizedList {
     private String email;
 
+    /*
+        @description: Constructor to initialize the email parameter.
+        @parameters:
+            - email: The email address to be tested.
+    */
     public EmailParameterizedList(String email){
         this.email = email;
     }
 
+    /*
+        @description: Provides a list of email addresses for parameterized testing.
+        @parameters: None
+        @return: Returns a list of email addresses as test data.
+    */
     @Parameters
     public static List<Object[]> data() {
         return Arrays.asList(new Object[][]{
@@ -25,11 +38,18 @@ public class EmailParameterizedList {
         });
     }
 
+    /*
+        @description: Tests the email validation using parameterized test data.
+        @parameters: None
+        @return: None
+    */
     @Test
     public void testEmailValidation(){
+        /*
+            @description: Validates the provided email address.
+            @parameters: None
+            @return: Expects the validation result to be true.
+        */
         Assert.assertTrue(EmailValidator.isValidateEmail(email));
     }
-
 }
-
-
