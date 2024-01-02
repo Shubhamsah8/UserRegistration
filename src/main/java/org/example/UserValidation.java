b7a1d21fd308f4986d57f3c0ec152e9bbe7e3249
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2024-01-02 09:17:30 +0530

    Update UserValidation.java

--- src/main/java/org/example/UserValidation.java
+++ src/main/java/org/example/UserValidation.java
@@ -1,20 +1,46 @@
 package org.example;
 
+/*
+    @description: This class handles user validation operations.
+*/
 public class UserValidation {
 
-    //First Use case of valid First name
+    /*
+        @description: Validates the first name according to specified criteria.
+        @parameters:
+            - firstName: The first name to be validated.
+        @return: Returns true if the first name is valid, false otherwise.
+    */
     public boolean isValidFirstName(String firstName){
         return firstName.matches("[A-Z]{1}[a-z]{2,}");
     }
 
+    /*
+        @description: Validates the last name according to specified criteria.
+        @parameters:
+            - lastName: The last name to be validated.
+        @return: Returns true if the last name is valid, false otherwise.
+    */
     public boolean isValidLastName(String lastName){
         return lastName.matches("[A-Z][a-z]{2,}");
     }
 
+    /*
+        @description: Validates an email address according to specified criteria.
+        @parameters:
+            - email: The email address to be validated.
+        @return: Returns true if the email address is valid, false otherwise.
+    */
     public boolean isValidEmail(String email){
         return email.matches("[a-zA-Z0-9./%]+@[A-Za-z]+\\.[a-zA-Z]{2,}");
     }
 
+    /*
+        @description: Validates a mobile number according to specified criteria.
+        @parameters:
+            - mobileNumber: The mobile number to be validated.
+        @return: Returns true if the mobile number is valid, false otherwise.
+    */
     public static boolean isValidMobileNumber(String mobileNumber){
         return mobileNumber.matches("91 \\d{10}");
     }
