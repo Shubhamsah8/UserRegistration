e9f8d373eaaeb2a3048a8c2b39c34009306a3248
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2024-01-07 20:26:35 +0530

    Add files via upload

--- src/test/java/EmailParameterizedList.java
+++ src/test/java/EmailParameterizedList.java
@@ -1,4 +1,4 @@
-import org.example.EmailValidator;
+import org.example.UserValidation;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.Assert;
@@ -8,14 +8,27 @@ import org.junit.runners.Parameterized.Parameters;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Parameterized test class for email validation using JUnit's Parameterized runner.
+ */
 @RunWith(Parameterized.class)
 public class EmailParameterizedList {
     private String email;
 
-    public EmailParameterizedList(String email){
+    /**
+     * Constructor for the EmailParameterizedList class.
+     *
+     * @param email the email to be tested
+     */
+    public EmailParameterizedList(String email) {
         this.email = email;
     }
 
+    /**
+     * Provides a list of email addresses as parameters for the parameterized test.
+     *
+     * @return a list of Object arrays containing email addresses
+     */
     @Parameters
     public static List<Object[]> data() {
         return Arrays.asList(new Object[][]{
@@ -25,11 +38,17 @@ public class EmailParameterizedList {
         });
     }
 
+    // Instance of UserValidation for testing
+    UserValidation userValidation = new UserValidation();
+
+    /**
+     * Parameterized test case for validating email addresses.
+     *
+     * @throws UserValidation.InvalidUserException if the email validation fails
+     */
     @Test
-    public void testEmailValidation(){
-        Assert.assertTrue(EmailValidator.isValidateEmail(email));
+    public void testEmailValidation() throws UserValidation.InvalidUserException {
+        // Assert that the email address is considered valid
+        Assert.assertTrue(userValidation.isValidEmail(email));
     }
-
 }
-
-
--- src/test/java/UserValidationTest.java
+++ src/test/java/UserValidationTest.java
@@ -1,55 +1,81 @@
 import org.example.UserValidation;
-import org.example.*;
 import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.Assert.*;
+
+/**
+ * Test class for the UserValidation class, covering various validation scenarios.
+ */
 public class UserValidationTest {
 
+    // Instance of UserValidation for testing
     UserValidation userValidation = new UserValidation();
+
+    /**
+     * Test case for validating a valid first name.
+     */
     @Test
-    public void testValidFirstName(){
+    public void testValidFirstName() {
         try {
+            // Assert that the first name "Shubham" is considered valid
             Assert.assertTrue(userValidation.isValidFirstName("Shubham"));
         } catch (UserValidation.InvalidUserException e) {
+            // Throw a RuntimeException if an unexpected exception occurs during the test
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * Test case for validating a valid last name.
+     */
     @Test
-    public void testValidLastName(){
-
+    public void testValidLastName() {
         try {
+            // Assert that the last name "Shah" is considered valid
             Assert.assertTrue(userValidation.isValidLastName("Shah"));
         } catch (UserValidation.InvalidUserException e) {
+            // Throw a RuntimeException if an unexpected exception occurs during the test
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * Test case for validating a valid email.
+     */
     @Test
-    public void testValidEmail(){
-
+    public void testValidEmail() {
         try {
+            // Assert that the email "shubhamsah086@gmail.com" is considered valid
             Assert.assertTrue(userValidation.isValidEmail("shubhamsah086@gmail.com"));
         } catch (UserValidation.InvalidUserException e) {
+            // Throw a RuntimeException if an unexpected exception occurs during the test
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * Test case for validating a valid mobile number.
+     */
     @Test
-    public void testValidMobileNumber(){
+    public void testValidMobileNumber() {
         try {
+            // Assert that the mobile number "91 8779817254" is considered valid
             Assert.assertTrue(userValidation.isValidMobileNumber("91 8779817254"));
         } catch (UserValidation.InvalidUserException e) {
+            // Throw a RuntimeException if an unexpected exception occurs during the test
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * Test case for validating a valid password.
+     */
     @Test
-    public void testValidPassword(){
+    public void testValidPassword() {
         try {
-            Assert.assertTrue(userValidation.isValidPassword("SHubhamsha123#"));
+            // Assert that the password "SHubhamsha123#" is considered valid
+            Assert.assertTrue(userValidation.isValidPassword("SHubhamsha123&"));
         } catch (UserValidation.InvalidUserException e) {
+            // Throw a RuntimeException if an unexpected exception occurs during the test
             throw new RuntimeException(e);
         }
     }
